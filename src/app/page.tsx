f5f988ccfd0c0fa6bc488a1131962dcd5ff7dddf
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-05-25 21:35:51 +0300

    написал всю базовую логику прилоения

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,48 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import { ChangeEvent, useState } from 'react';
+import './globals.css'
+
+interface Item {
+	id: number;
+	value: string;
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [newItem, setNewItem] = useState<string>('');
+	const [itemsList, setItemsList] = useState<Item[]>([])
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setNewItem(e.target.value);
+	};
+	
+	const addNewItemToList = () => {
+		const item = {
+			id: Math.floor(Math.random() * 1000),
+			value: newItem
+		}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+		setItemsList(prev => [...prev, item])
+		setNewItem('')
+	}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+	const deleteFromlist = (id: number) => {
+		const newArray = itemsList.filter(item => item.id !== id);
+		setItemsList(newArray)
+	}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+	return (
+		<main>
+			<h1>TODOList</h1>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+			<input type="text" value={newItem} onChange={handleInputChange} />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+			<button onClick={addNewItemToList}>добавить</button>
+
+			<ul className='flex item-list'>
+			{itemsList.map(item => (
+				
+				<li key={item.id} >{item.value}  <button onClick={() => deleteFromlist(item.id)}>удалить</button></li> 
+			)) }
+			</ul>
+		</main>
+	);
 }
